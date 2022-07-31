594e20c3e36f47c64e502134ddcb1b9107188b45
Leon68 <leon-dev@ukr.net>
2022-07-31 18:37:41 +0300

    add ver.2

---
+++ RunAsAdmV2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Security;
+
+Console.WriteLine("Run as administrator");
+Console.WriteLine("Run this program with 3 params");
+Console.WriteLine("   1 - The Name of a local adminstrator (adminstrator)");
+Console.WriteLine("   2 - The Password of the local adminstrator");
+Console.WriteLine("   3 - The Full Path and name of a program which do you want to run");
+Console.WriteLine();
+
+try
+{
+    string username = args[0];
+    string ps = args[1];
+
+    char[] charArr = ps.ToCharArray();
+
+    SecureString userpass = new SecureString();
+
+    foreach (var item in charArr)
+        userpass.AppendChar(item);
+
+    var psi = new ProcessStartInfo
+    {
+        FileName = args[2],
+        UserName = username,
+        Domain = "",
+        Password = userpass,
+        UseShellExecute = false,
+        RedirectStandardOutput = true,
+        RedirectStandardError = true
+    };
+
+    Process.Start(psi);
+
+    Console.WriteLine();
+    Console.WriteLine($"Run application - {args[2]}");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error message - {ex.Message}");
+    Console.ReadLine();
+}
